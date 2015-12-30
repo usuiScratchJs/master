74266199c111953266698637c60e83ea30e0a273
菅 真梨子 <suga.mariko51089@gmail.com>
2015-12-30 22:26:58 +0900

    課題1修正

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,26 +1,26 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 'use strict';
 
-var i = document.querySelectorAll('.button');
-var o = document.querySelectorAll('.cont');
-//var a = document.querySelectorAll('active');
+var tabSwitch = function tabSwitch() {
+    var $trigger = document.querySelectorAll('.button');
+    var $target = document.querySelectorAll('.cont');
+    var $active;
+    for (var num = 0, length = $trigger.length; num < length; num++) {
+        $target[num].classList.add('none');
+        $target[0].classList.remove('none');
+        $target[0].classList.add('active');
+        (function (index) {
+            $trigger[num].addEventListener("click", function () {
+                $active = document.querySelector('.active');
+                $active.classList.add('none');
+                $active.classList.remove('active');
+                $target[index].classList.add('active');
+                $target[index].classList.remove('none');
+            });
+        })(num);
+    }
+};
 
-for (var num = 0, length = i.length; num < length; num++) {
-    o[num].classList.add('none');
-    o[0].classList.remove('none');
-    o[0].classList.add('active');
-    (function (art) {
-        i[num].addEventListener("click", function () {
-            //a.classList.remove('active');
-            //a.classList.add('none');
-            //o[0].classList.remove('active');
-            o[0].classList.add('none');
-            o[1].classList.add('none');
-            o[2].classList.add('none');
-            o[art].classList.remove('none');
-            o[art].classList.add('active');
-        }, false);
-    })(num);
-}
+tabSwitch();
 
 },{}]},{},[1]);
--- src/js/app.js
+++ src/js/app.js
@@ -1,23 +1,23 @@
-var i = document.querySelectorAll('.button');
-var o = document.querySelectorAll('.cont');
-//var a = document.querySelectorAll('active');
+var tabSwitch = function() {
+    var $trigger = document.querySelectorAll('.button');
+    var $target = document.querySelectorAll('.cont');
+    var $active;
+    for (var num = 0, length = $trigger.length; num < length; num++) {
+        $target[num].classList.add('none');
+        $target[0].classList.remove('none');
+        $target[0].classList.add('active');
+        (function (index) {
+            $trigger[num].addEventListener("click", function(){
+                $active = document.querySelector('.active');
+                $active.classList.add('none');
+                $active.classList.remove('active');
+                $target[index].classList.add('active');
+                $target[index].classList.remove('none');
+            });
+        })(num);
+    }
+};
 
+tabSwitch();
 
-for(var num = 0, length = i.length; num < length; num++) {
-    o[num].classList.add('none');
-    o[0].classList.remove('none');
-    o[0].classList.add('active');
-    (function (art) {
-        i[num].addEventListener("click", function () {
-            //a.classList.remove('active');
-            //a.classList.add('none');
-            //o[0].classList.remove('active');
-            o[0].classList.add('none');
-            o[1].classList.add('none');
-            o[2].classList.add('none');
-            o[art].classList.remove('none');
-            o[art].classList.add('active');
-        }, false);
-    })(num);
-}
 
